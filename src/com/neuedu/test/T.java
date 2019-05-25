9994e6a7e1e516fa490abd37fabb517cafd6f738
Administrator <1161848656@qq.com>
2019-05-25 15:15:19 +0800

    fgfdg

--- src/com/neuedu/test/T.java
+++ src/com/neuedu/test/T.java
@@ -1,6 +1,6 @@
 package com.neuedu.test;
 
-public class Test {
+public class T {
     public static void main(String[] args) {
         System.out.println(1);
     }//https://github.com/Strong7841/javaBasic.git
